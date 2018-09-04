26558d7fcec28043b343edca77819c6f50463e03
jap011 <josh.patton@nationwidechildrens.org>
2018-09-04 19:45:53 -0400

    Building out calculation logic for babysitter and Program.cs

--- BabysitterApp/src/BabysitterApp/Babysitter.cs
+++ BabysitterApp/src/BabysitterApp/Babysitter.cs
@@ -9,10 +9,11 @@ namespace BabysitterApp
     {
 
         //Variables needed for both times and probably for bedTime and possibly storing midnight
-        DateTime startTime;
-        DateTime endTime;
-        DateTime bedTime;
-        DateTime midnight;
+        private DateTime startTime;
+        private DateTime endTime;
+        
+        private DateTime bedTime;
+        private DateTime midnight;
 
         double[] CostTime = { 12, 8, 16 };
         TimeSpan[] TotalTime = new TimeSpan[3];
@@ -26,24 +27,29 @@ namespace BabysitterApp
             //how much time is between 9pm and midnight
             //how much time is between midnight and endTime
 
-            //Assume babysitter always stays until bedtime
+            //Assume babysitter always stays until bedtime, bedtime is 9pm for now
+            bedTime = DateTime.Today.AddHours(21);
+            midnight = DateTime.Today.AddHours(24);
 
-
-            TotalTime[0] = this.bedTime - this.startTime;
+            TotalTime[0] = this.bedTime - startTime;
                         
 
             //Beginning of logic to calculate these three values
-            if (endTime >= midnight)
+            if (EndTime >= midnight)
             {
                 TotalTime[2] = endTime - midnight;
                 TotalTime[1] = midnight - bedTime;
 
             } else if (endTime < midnight)
             {
-
+                TotalTime[1] = endTime - bedTime;
             }
-            
-            return 0.0;
+
+            var c1 = (TotalTime[0].TotalHours * CostTime[0]);
+            var c2 = (TotalTime[1].TotalHours * CostTime[1]);
+            var c3 = (TotalTime[2].TotalHours * CostTime[2]);
+
+            return c1 + c2 + c3;
 
         }
 
@@ -66,6 +72,22 @@ namespace BabysitterApp
             //Are both times valid
             return true;
         }
+
+
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+
+
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+
+
 
     }
 }
